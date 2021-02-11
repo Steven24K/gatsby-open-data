92df1beee87a0995f4e268b0a69308e33bb0dd42
Steven <s.koerts2@gmail.com>
2021-02-11 15:40:44 +0100

    Remove react bootstrap

--- src/pages/wiki.jsx
+++ src/pages/wiki.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import Layout from "../layout";
 import config from "../../data/SiteConfig";
-import { FormControl, InputGroup, Form } from "react-bootstrap";
 
 const available_languages = [
     'en',
@@ -32,29 +31,13 @@ function WikiPage() {
                 <h1>Welcome to Wikipedia</h1>
                 <h2>The largest free wiki!</h2>
 
-                <InputGroup>
-                    <FormControl
-                        value={searchQuery} onChange={e => setQuery(e.target.value)}
-                        placeholder="Search the Wiki"
-                        aria-label="Username"
-                        aria-describedby="basic-addon1"
-                    />
-                    <InputGroup.Prepend>
-                        <InputGroup.Checkbox aria-label="Checkbox for following text input" checked={useCustomLang} onChange={() => toggleCustimLang(!useCustomLang)} />
-                    </InputGroup.Prepend>
-                    {useCustomLang && <FormControl aria-label="Text input with checkbox" value={customLang} onChange={e => setCustomLang(e.target.value)} />}
-                    <Form.Group controlId="exampleForm.ControlSelect1"  >
-                        <Form.Label>Example select</Form.Label>
-                        <Form.Control as="select" onChange={e => setLang(e.target.value)}>
-                            {available_languages.map(lan => <option key={lan} value={lan}>{lan}</option>)}
-                        </Form.Control>
-                    </Form.Group>
-                </InputGroup>
-
-
+                <input value={searchQuery} onChange={e => setQuery(e.target.value)} placeholder="Search the Wiki" />
+                Custom language: <input checked={useCustomLang} onChange={() => toggleCustimLang(!useCustomLang)} />
+                {useCustomLang && <input value={customLang} onChange={e => setCustomLang(e.target.value)} />}
                 <select onChange={e => setLang(e.target.value)}>
                     {available_languages.map(lan => <option key={lan} value={lan}>{lan}</option>)}
                 </select>
+
 
                 <div>
                     <iframe src={`https://${(useCustomLang && customLang != "") ? customLang : lang}.wikipedia.org/wiki/${searchQuery}`} width={screen.width * 0.8} height={screen.height} />
